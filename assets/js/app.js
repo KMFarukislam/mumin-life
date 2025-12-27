3b4be886260527b87c1127952c4648d39f1bd419
K M Faruk islam <49297348+KMFarukislam@users.noreply.github.com>
2025-12-27 16:18:04 +0600

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,21 @@
+const toggle = document.getElementById("themeToggle");
+const body = document.body;
 
+// Load saved theme
+if (localStorage.getItem("theme") === "light") {
+  body.classList.add("light");
+  toggle.textContent = "☀️";
+}
+
+// Toggle theme
+toggle.onclick = () => {
+  body.classList.toggle("light");
+
+  if (body.classList.contains("light")) {
+    localStorage.setItem("theme", "light");
+    toggle.textContent = "☀️";
+  } else {
+    localStorage.setItem("theme", "dark");
+    toggle.textContent = "🌙";
+  }
+};
